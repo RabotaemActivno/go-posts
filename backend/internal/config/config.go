a9525fba29ba88577d5b882a3113c46ea3d1529f
ifayzov <ifayzov@kazanworkout.ru>
2025-11-28 18:35:37 +0300

    add config file

---
+++ backend/internal/config/config.go
@@ -0,0 +1,42 @@
+package config
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Env string `yaml:"env" env-default:"local"`
+	StoragePath string `yaml:"storage_path" env_required:"true"`
+	HTTPServer `yaml:"http_server"`
+}
+
+type HTTPServer struct {
+	Address string `yaml:"address" env-default:"localhost:8080"`
+	Timeout time.Duration `yaml:"timeout" env-default:"4s"`
+}
+
+func MustLoad() *Config {
+
+	os.Setenv("CONFIG_PATH", "config/local.yaml")
+
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatalf("CONFIG_PATH is not set")
+	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("config file %s does not exists", configPath)
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalf("can not read confg %s", err)
+	}
+	
+	return &cfg
+}
