fd4d0c9f19503f6cfa5f5e4d684a4c58443baf79
ifayzov <ifayzov@kazanworkout.ru>
2025-12-02 15:36:41 +0300

    add ui components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
+import List from "./components/list/List"
+import Navbar from "./layouts/Navbar"
 
 function App() {
 
   return (
-    <>
-    </>
+    <div>
+      <Navbar/>
+      <List/>
+    </div>
   )
 }
 
